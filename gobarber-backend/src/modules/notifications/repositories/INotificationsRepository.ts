f68290eb35d391bc1750ae9df7b67b5a17ac83fe
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2021-04-27 13:25:36 -0300

    Done notifications

---
+++ gobarber-backend/src/modules/notifications/repositories/INotificationsRepository.ts
@@ -0,0 +1,7 @@
+import { create } from 'handlebars';
+import ICreateNotificationDTO from '../dtos/ICreateNotificationDTO';
+import Notification from '../infra/typeorm/schemas/Notification';
+
+export default interface INotificationsRepository {
+  create(data: ICreateNotificationDTO): Promise<Notification>;
+}
